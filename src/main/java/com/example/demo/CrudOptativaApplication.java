c8051e8457a2e98b1d048940ff7a13c206e39b8f
Alejandro Campos <alejandrocampossampedro@gmail.com>
2026-01-21 14:12:50 +0100

    "Trabajo creado "

---
+++ src/main/java/com/example/demo/CrudOptativaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOptativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOptativaApplication.class, args);
+	}
+
+}
